ed2adb21cf557cf892b43ce1918ff74d47d36243
Learn2see <ariyapong@juno.com>
2017-08-21 12:21:28 -0400

    Made code changes to align with class instruction.
    Misunderstood the original direction.  This should address the issue.

--- cachematrix.R
+++ cachematrix.R
@@ -2,35 +2,39 @@
 ## in a different environment than the current environment.  It creates a "special vector".
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
+        inv <- NULL
         set <- function(y) {
                 x <<- y
-                m <<- NULL
+                inv <<- NULL
                 
         }
-        get <- function() x
-        setmean <- function(mean) m <<- mean()
-        getmean <- function() m
+
+      # Setup the function and returns matix  
+        get <- function() 
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
         list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## This second function calculates the mean of the vector created in the first function.
-## It first checks to see if the mean has been previously calculated.  If so it pulls the value
-## from the cache.  If not it calculates the mean and set the result in the cache.
+## This second function calculates the inverse matrix of the first function.
+## It first checks to see if the matrix has been previously calculated.  If so it pulls the value
+## from the cache.  If not it calculates the inverse and set the result in the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         
-        m <- x$getmean()
-        if(!is.null(m)) {
+        m <- x$getmean(x,...)
+        inv <- x$getinverse()
+        
+        if(!is.null(inv)) {
                 message("getting cached data")
                 return(m)
         }
         data <- x$get()
-        m <- mean(data, ...)
-        x$mean(m)
-        m
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
